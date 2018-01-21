34361276214daa2b46ccce008100caac304446f4
Nikita Zadiriushko <nikitazadiriuschko@yandex.ru>
2018-01-21 07:51:02 -0800

    Milestone 6 done

--- src/main/scala/observatory/Interaction2.scala
+++ src/main/scala/observatory/Interaction2.scala
@@ -5,11 +5,31 @@ package observatory
   */
 object Interaction2 {
 
+  val temperatures: List[PalettePoint] = List(
+    (60, Color(255, 255, 255)),
+    (32, Color(255, 0, 0)),
+    (12, Color(255, 255, 0)),
+    (0, Color(0, 255, 255)),
+    (-15, Color(0, 0, 255)),
+    (-27, Color(255, 0, 255)),
+    (-50, Color(33, 0, 107)),
+    (-60, Color(0, 0, 0)))
+
+  val deviations: List[PalettePoint] = List(
+    (7, Color(0, 0, 0)),
+    (4, Color(255, 0, 0)),
+    (2, Color(255, 255, 0)),
+    (0, Color(255, 255, 255)),
+    (-2, Color(0, 255, 255)),
+    (-7, Color(0, 0, 255)))
+
   /**
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    Seq(
+      Layer(LayerName.Temperatures, temperatures, 1975 to 2015),
+      Layer(LayerName.Deviations, temperatures, 1990 to 2015))
   }
 
   /**
@@ -22,7 +42,7 @@ object Interaction2 {
 
   /**
     * @param selectedLayer The selected layer
-    * @param sliderValue The value of the year slider
+    * @param sliderValue   The value of the year slider
     * @return The value of the selected year, so that it never goes out of the layer bounds.
     *         If the value of `sliderValue` is out of the `selectedLayer` bounds,
     *         this method should return the closest value that is included
@@ -39,7 +59,7 @@ object Interaction2 {
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
@@ -48,25 +68,28 @@ object Interaction2 {
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Year]): Signal[String] = {
-    ???
+    Signal(s"${selectedLayer().layerName.id.capitalize} (${selectedYear()})")
   }
-
 }
 
 sealed abstract class LayerName(val id: String)
+
 object LayerName {
+
   case object Temperatures extends LayerName("temperatures")
+
   case object Deviations extends LayerName("deviations")
+
 }
 
 /**
-  * @param layerName Name of the layer
+  * @param layerName  Name of the layer
   * @param colorScale Color scale used by the layer
-  * @param bounds Minimum and maximum year supported by the layer
+  * @param bounds     Minimum and maximum year supported by the layer
   */
 case class Layer(layerName: LayerName, colorScale: Seq[(Temperature, Color)], bounds: Range)
 
--- src/test/scala/observatory/Interaction2Test.scala
+++ src/test/scala/observatory/Interaction2Test.scala
@@ -6,27 +6,27 @@ import org.scalatest.prop.Checkers
 
 class Interaction2Test extends FunSuite with Checkers {
 
+  val colors: Seq[(Temperature, Color)] = Array((-20.0, Color(0, 0, 255)), (10.0, Color(50, 50, 100)))
+  val layer = Layer(Temperatures, colors, (1970 to 1990))
+
   test("yearBounds should be calculated correctly") {
-    val colors: Seq[(Temperature, Color)] = Array((-20.0, Color(0, 0, 255)), (10.0, Color(50, 50, 100)))
-    val layer = Layer(Temperatures, colors, (0 until 10))
-    assert(Interaction2.yearBounds(Signal(layer))() === Signal((0 until 10))())
+    val customLayer = Layer(Temperatures, colors, (0 until 10))
+    assert(Interaction2.yearBounds(Signal(customLayer))() === Signal((0 until 10))())
   }
 
   test("yearSelection should be calculated correctly #1") {
-    val colors: Seq[(Temperature, Color)] = Array((-20.0, Color(0, 0, 255)), (10.0, Color(50, 50, 100)))
-    val layer = Layer(Temperatures, colors, (1970 to 1990))
     assert(Interaction2.yearSelection(Signal(layer), Signal(1980))() == Signal(1980)())
   }
 
   test("yearSelection should be calculated correctly #2") {
-    val colors: Seq[(Temperature, Color)] = Array((-20.0, Color(0, 0, 255)), (10.0, Color(50, 50, 100)))
-    val layer = Layer(Temperatures, colors, (1970 to 1990))
-    assert(Interaction2.yearSelection(Signal(layer), Signal(1600))() == Signal(1970)())
+     assert(Interaction2.yearSelection(Signal(layer), Signal(1600))() == Signal(1970)())
   }
 
   test("yearSelection should be calculated correctly #3") {
-    val colors: Seq[(Temperature, Color)] = Array((-20.0, Color(0, 0, 255)), (10.0, Color(50, 50, 100)))
-    val layer = Layer(Temperatures, colors, (1970 to 1990))
     assert(Interaction2.yearSelection(Signal(layer), Signal(2000))() == Signal(1990)())
+  }
+
+  test("caption method should return correct string") {
+    assert(Interaction2.caption(Signal(layer), Signal(1980))() === "Temperatures (1980)")
   }
 }
